2dc8567aacac1ba194cfe3d2b56682300f9263d5
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-05 12:41:58 -0700

    Updating script

--- exp/mob_init/run.sh
+++ exp/mob_init/run.sh
@@ -8,3 +8,8 @@ source /home/chauvec/projects/ctb-chauvec/PLASMIDS/ARETE_MAY22/hyasp_env/bin/act
 module load StdEnv/2020  gcc/9.3.0 blast+/2.12.0 mash
 
 mob_init
+
+# Fixing apparentbug
+makeblastdb -in /project/6069942/PLASMIDS/ARETE_MAY22/hyasp_env/lib/python3.6/site-packages/mob_suite-3.0.3-py3.6.egg/mob_suite/databases/rep.dna.fas -dbtype nucl
+makeblastdb -in /project/6069942/PLASMIDS/ARETE_MAY22/hyasp_env/lib/python3.6/site-packages/mob_suite-3.0.3-py3.6.egg/mob_suite/databases/orit.fas -dbtype nucl
+
