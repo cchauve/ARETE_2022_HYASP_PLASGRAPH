c1b51679d49796c65eedf1c64c66a9e5466cee8b
Cedric Chauve <chauvec@cedar5.cedar.computecanada.ca>
2022-05-12 10:27:24 -0700

    Cleaning unused experiments

--- exp/e_coli_mob/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SAMPLES=`cat /home/chauvec/projects/ctb-chauvec/PLASMIDS/ARETE_MAY22/data/e_coli/e_coli_id.txt`
-
-for SAMPLE in ${SAMPLES}
-do
-    sed "s/SAMPLE_ID/${SAMPLE}/g" run_template.sh > scripts/run_${SAMPLE}.sh
-    sbatch scripts/run_${SAMPLE}.sh
-done
