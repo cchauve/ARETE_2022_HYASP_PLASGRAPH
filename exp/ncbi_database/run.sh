80aa039c9ade7ef91f507a4a4e28ff3a7e531896
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-12 20:47:54 -0700

    database ncbi

--- exp/ncbi_database/run.sh
+++ exp/ncbi_database/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #SBATCH --time=8:00:00
-#SBATCH --account=def-chauvec
 #SBATCH --mem=8G
-source /home/chauvec/projects/ctb-chauvec/PLASMIDS/ARETE_MAY22/hyasp_env/bin/activate 
-cd /home/chauvec/projects/ctb-chauvec/PLASMIDS/ARETE_MAY22/exp/ncbi_database
+#SBATCH --account=def-chauvec
+
+# Experiments home directory
+ARETE_MAY22_HOME=/home/chauvec/projects/ctb-chauvec/PLASMIDS/ARETE_MAY22/
+
+# Virtual environment home directory
+HYASP_ENV_HOME=${ARETE_MAY22_HOME}/hyasp_env/
+source ${HYASP_ENV_HOME}/bin/activate
+
 hyasp create ncbi_database_genes.fasta -p plasmids.csv -k ncbi_database_plasmids.fa -b ncbi_blacklist.txt -d -l 500 -m 100 -t GenBank -v  > log_2022-05-04_1.txt
