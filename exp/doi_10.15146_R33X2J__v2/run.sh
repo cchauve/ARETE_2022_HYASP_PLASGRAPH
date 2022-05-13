207b47ef8676b5ce4e8dc10867a9f4dd43e48575
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-12 20:46:33 -0700

    database doi_10.15146_R33X2J_

--- exp/doi_10.15146_R33X2J__v2/run.sh
+++ exp/doi_10.15146_R33X2J__v2/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #SBATCH --time=8:00:00
+#SBATCH --mem=8G
 #SBATCH --account=def-chauvec
+
+# Experiments home directory
+ARETE_MAY22_HOME=/home/chauvec/projects/ctb-chauvec/PLASMIDS/ARETE_MAY22/
+
+# Virtual environment home directory
+HYASP_ENV_HOME=${ARETE_MAY22_HOME}/hyasp_env/
+source ${HYASP_ENV_HOME}/bin/activate
+
 hyasp create doi_10.15146_R33X2J__v2_genes.fasta -a doi_10.15146_R33X2J__v2_id.txt -k doi_10.15146_R33X2J__v2_plasmids.fa -d -l 500 -m 100 -v 
