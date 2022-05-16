ceb7e514b0c90dbdcfcb1ad23c86c5cbf1a3d88f
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-16 11:16:02 -0700

    home dir

--- scripts/hyasp_install.sh
+++ scripts/hyasp_install.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem=8G
 #SBATCH --account=def-chauvec
 
-# To edit to go into the directory where you want to install the environment
-cd /home/chauvec/projects/ctb-chauvec/PLASMIDS/ARETE_MAY22
+source ../home.sh
+cd ${ARETE_MAY22_HOME}
 
 module load python/3
 python3 -m venv --system-site-packages ./hyasp_env
--- scripts/run_filter_hyasp_questionable_plasmids.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./filter_hyasp_questionable_plasmids.sh ../exp/e_coli_hyasp_doi_10.15146_R33X2J__v2/ ../data/e_coli/e_coli_id.txt
-./filter_hyasp_questionable_plasmids.sh ../exp/e_coli_hyasp_ncbi/ ../data/e_coli/e_coli_id.txt
-./filter_hyasp_questionable_plasmids.sh ../exp/e_faecium_hyasp_ncbi/ ../data/e_faecium/e_faecium_id.txt
-./filter_hyasp_questionable_plasmids.sh ../exp/e_faecium_hyasp_doi_10.15146_R33X2J__v2/ ../data/e_faecium/e_faecium_id.txt
