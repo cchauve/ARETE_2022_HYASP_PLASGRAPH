138a9562cb4d82676de9e13121bb4eb83b6074c0
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-16 14:06:17 -0700

    plasgraph eval script

--- exp/c_freundii_plasgraph/run_eval.sh
+++ exp/c_freundii_plasgraph/run_eval.sh
@@ -4,4 +4,4 @@ source ../../home.sh
 
 module load python/3
 
-python3 ../../scripts/eval_plasgraph.py ${ARETE_MAY22_HOME} c_freundii
+python3 ${ARETE_MAY22_HOME}/scripts/eval_plasgraph.py ${ARETE_MAY22_HOME} c_freundii
