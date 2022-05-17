a8f0725679ba756d2dca80b6d232f615426f576e
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-16 23:17:56 -0700

    run scripts

--- exp/c_freundii_plasgraph/run_template.sh
+++ exp/c_freundii_plasgraph/run_template.sh
@@ -14,14 +14,18 @@ source ${PLASGRAPH_ENV_HOME}/bin/activate
 # Going into the plasgraph directory
 cd ${ARETE_MAY22_HOME}/tools/plASgraph
 
+# Experiment directory
+EXP_DIR=${ARETE_MAY22_HOME}/exp/c_freundii_plasgraph
 # Input
 DATA_DIR=${ARETE_MAY22_HOME}/data/c_freundii/assembly_graphs
-INPUT=${DATA_DIR}/SAMPLE_ID.gfa
-gunzip ${INPUT}.gz
+mkdir -p ${EXP_DIR}/tmp
+cp ${DATA_DIR}/SAMPLE_ID.gfa.gz ${EXP_DIR}/tmp
+gunzip ${EXP_DIR}/tmp/SAMPLE_ID.gfa.gz
+GFA=${EXP_DIR}/tmp/SAMPLE_ID.gfa
 
 # Running plASgraph
 OUT_DIR=${ARETE_MAY22_HOME}/exp/c_freundii_plasgraph/results/SAMPLE_ID
 mkdir -p ${OUT_DIR}
-python plASgraph.py -i ${INPUT} -o ${OUT_DIR}/SAMPLE_ID_class.csv --draw_graph
+python plASgraph.py -i ${GFA} -o ${OUT_DIR}/SAMPLE_ID_class.csv --draw_graph
 
-gzip ${INPUT}
+rm ${GFA}
