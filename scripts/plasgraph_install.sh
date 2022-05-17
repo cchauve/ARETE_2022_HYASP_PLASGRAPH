83104d4962f6382387353921622a831f322ed27f
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2022-05-16 19:02:40 -0700

    plasgraph install

--- scripts/plasgraph_install.sh
+++ scripts/plasgraph_install.sh
@@ -9,14 +9,14 @@ module load python/3
 python3 -m venv --system-site-packages ${ARETE_MAY22_HOME}/plasgraph_env
 source ${ARETE_MAY22_HOME}/plasgraph_env/bin/activate
 
-pip install networkx==2.6.3
-pip install pandas==1.4.0
-pip install numpy==1.22.2
-pip install scikit-learn==0.23.1
-pip install biopython==1.79
-pip install matplotlib==3.5.1
-pip install --no-index tensorflow==2.8
-pip install spektral==1.0.8
+pip install 'networkx>=2.6.3'
+pip install 'pandas>=1.3.5'
+pip install 'numpy>=1.21.5'
+pip install 'scikit-learn>=0.23.1'
+pip install 'biopython>=1.79'
+pip install 'matplotlib>=3.5.1'
+pip install --no-index 'tensorflow>=2.8'
+pip install 'spektral>=1.0.8'
 
 mkdir -p ${ARETE_MAY22_HOME}/tools
 cd ${ARETE_MAY22_HOME}/tools/
